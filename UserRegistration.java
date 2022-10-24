a59d45d4e2a19e628c9f99ebed49c679f81819b5
zhoffman1 <zfh1@pitt.edu>
2022-10-24 15:30:29 -0400

    Create user registration program outline

---
+++ UserRegistration.java
@@ -0,0 +1,22 @@
+/** 
+ * Program to register users to the system
+ * @author  Zachary Hoffman
+ */
+import java.io.File;
+import java.util.Scanner;
+
+public class UserRegistration {
+    public static void main (String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        
+        // create file to store usernames/passwords (if not already exist)
+
+        // read user input
+
+        // get hash of password
+        
+        // store username and hashed password in file (username not already exist)
+
+        // 
+    }
+}
