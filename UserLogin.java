ef3a02e9b1ed0a63bdd85157648aa412c7936ffc
zhoffman1 <zfh1@pitt.edu>
2022-10-24 15:44:03 -0400

    Create user login program

---
+++ UserLogin.java
@@ -0,0 +1,12 @@
+/** 
+ * Program for users to log in to the system
+ * @author  Zachary Hoffman
+ */
+import java.io.File;
+import java.util.Scanner;
+
+public class UserLogin {
+    public static void main (String[] args) {
+        
+    }
+}
