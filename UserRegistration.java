70233065f027a6eefeffcadf2772807d6afd1a6e
zhoffman1 <zfh1@pitt.edu>
2022-10-25 11:21:43 -0400

    More comments and outline TODO list

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,15 +9,34 @@ import java.util.Scanner;
 public class UserRegistration {
     public static void main (String[] args) {
         Scanner kbd = new Scanner(System.in);
-        
+        boolean exists = false;
+        String username;
         // create file to store usernames/passwords (if not already exist)
-
+        
         // read user input
+        do {
+            if (exists == true) {
+                System.out.println("Username already exists. Please enter a new username: ");
+            }
+            else {
+                System.out.println("Create username: ");
+            }
+
+            username = kbd.nextLine();
+            exists = false; // TODO: make this actually check the preexisting file
+                            // true if name already exists, false if not
+        } while (exists);
+
+        System.out.println("Create password: ");
 
         // get hash of password
         
         // store username and hashed password in file (username not already exist)
 
         // can probably store using a hashmap (makes .txt file unreadable)
+
+        System.out.println("Successfully created user " + username + ".");
+
+        // TODO: make the whole program loop so that the user can create multiple users in 1 go
     }
 }
