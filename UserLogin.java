f45c5e506c228f88b932c4290528b8d795fd351a
zhoffman1 <zfh1@pitt.edu>
2022-10-28 17:11:08 -0400

    Begin getUserInfo and verifyUser methods

--- UserLogin.java
+++ UserLogin.java
@@ -3,12 +3,41 @@
  * @author  Zachary Hoffman
  */
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 public class UserLogin {
     public static void main (String[] args) {
-        Scanner kbd = new Scanner(System.in); // keyboard reader
-        // need something to read file in (depends on storing as text or hashmap)
+        Scanner kbd = new Scanner(System.in);
+        // read hashmap in
+
+        // get user info
+        String[2] userInfo = getUserInfo(kbd);
+        
+        // check if username exists
+
+        
+            // hash the supplied password
+
+            // see if they match
+    }
+
+    /**
+     * Ask for user to input their username and password, return an array 
+     * containing their inputs
+     *
+     * @param   kbd scanner reading from keyboard (System.in)
+     * @return  String array containing the username input at index 0 and 
+     *          password input at index 1
+     *
+     */
+    public static String[] getUserInfo(Scanner kbd) {
+        String arr[] = new String[2];
 
         // ask for username
         System.out.println("Username: ");
@@ -18,10 +47,15 @@ public class UserLogin {
         System.out.println("Password: ");
         String password = kbd.nextLine();
 
-        // check if username exists
-        
-            // hash the supplied password
+        // store username and password in array
+        arr[0] = username;
+        arr[1] = password;
 
-            // see if they match
+        return arr;
+    }
+
+    // check whether this user actually exists and if their password is valid
+    public static boolean verifyUser(String username, String password, HashMap<String> hmap) {
+        
     }
 }
